ebca602c401105ab45688ba343879f8c20e03a71
Aaron Ledbetter <aaronledbetter@Aarons-Air.lan>
2022-03-31 10:31:38 -1000

    heroku build

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/character', characterController);
 
-const Port = process.env.PORT || 3000
+const Port = process.env.PORT || 3001
 app.listen(Port , ()=>{
-    console.log('expres===============>')
+    console.log('express')
 })
