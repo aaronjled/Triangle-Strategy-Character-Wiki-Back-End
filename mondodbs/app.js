86589a9d906c2c7dacfab0998b0c5b6fe47e3471
Aaron Ledbetter <aaronledbetter@Aarons-Air.lan>
2022-04-01 08:40:40 -1000

    possible fix

--- mondodbs/app.js
+++ mondodbs/app.js
@@ -4,7 +4,7 @@ const db = mongoose.connection;
 
 // Connect to Mongo
 mongoose
-    .connect( process.env.MONGODB_URI , {useNewUrlParser: true})
+    .connect( process.env.MONGODB_URI , {useNewUrlParser: true}, {retryWrites: false})
     .then(()=> console.log('mongo connected: ', process.env.MONGODB_URI))
     .catch((err) => console.log(err));
 
